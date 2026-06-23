a08e47b7a6e56cbb3baee6bec82af118ede43771
mpatidinov007 <mpatidinov007@gmail.com>
2026-06-23 22:46:07 +0500

    Fix react-joyride import - use named import

--- src/components/OnboardingTour.jsx
+++ src/components/OnboardingTour.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Joyride, { STATUS } from 'react-joyride';
+import { Joyride, STATUS } from 'react-joyride';
 import { motion } from 'framer-motion';
 import styled from 'styled-components';
 import { useOnboarding } from '../context/OnboardingContext';
